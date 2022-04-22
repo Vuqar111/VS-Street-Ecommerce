8782b1361a092e4d4a95af3bab0388593dd7879d
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2022-04-22 10:22:18 +0400

    removing something

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
-require("dotenv").config();
+
 
  
 
