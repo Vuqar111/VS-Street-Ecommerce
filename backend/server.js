642073c1a7b0218d6a15b221f7f212330c52d3b6
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2022-04-28 20:03:05 +0400

    ff

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,6 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
 // app.get('*', (req, res) =>
 //   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 // );
