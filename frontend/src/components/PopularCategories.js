a3cea72393ec337a420f78ab8e0415890216b6f8
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2022-05-05 00:34:27 +0400

    making more better

--- frontend/src/components/PopularCategories.js
+++ frontend/src/components/PopularCategories.js
@@ -3,12 +3,8 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import image1 from "../assets/image1.jpg";
 import image2 from "../assets/image2.jpg";
-import image3 from "../assets/image3.jpg";
-import image4 from "../assets/image4.jpg";
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-
+import tshirtvsfashion from '../assets/tshirtvsfashion.jpg';
+import tshirt2vsfashion from '../assets/tshirt2vsfashion.jpeg';
 const PopularCategories = () => {
   return (
     <Wrapper>
@@ -19,16 +15,19 @@ const PopularCategories = () => {
       </div>
       <div className="mainpopularsection">
         <div className="smalldiv m-[10px]"> 
-          <img src={image2} alt="image" />
-          <p className="subimglink">Explore More</p>
+          <img src={tshirtvsfashion} alt="image" />
+          <h3 className="subimglinkh3">Tshirt Kolleksiyası</h3>
+          <p className="subimglink">Kəşf Et</p>
         </div>
         <div className="smalldiv m-[10px]">
           <img src={image2} alt="image" />
-          <p className="subimglink">Explore More</p>
+          <h3 className="subimglinkh3">Hoodie Kolleksiyası</h3>
+          <p className="subimglink">Kəşf Et</p>
         </div>
         <div className="smalldiv m-[10px]">
-          <img src={image2} alt="image" />
-          <p className="subimglink">Explore More</p>
+          <img src={tshirt2vsfashion} alt="image" />
+          <h3 className="subimglinkh3">VsFashion Kolleksiyası</h3>
+          <p className="subimglink">Kəşf Et</p>
         </div>
       </div>
     </Wrapper>
@@ -44,6 +43,78 @@ const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    align-items: center;
+    margin-top: 40px;
+  }
+  .imagediv {
+    display: flex;
+    width: 50%;
+  }
+
+  .imagediv2 {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+  }
+
+  .smalldiv {
+    position: relative;
+    width: 300px;
+    height: 400px;
+    max-height: 600px;
+  }
+  .subimglinkh3 {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    font-size: 30px;
+    font-weight: bolder;
+  }
+  .subimglink {
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+    padding: 10px;
+    border-radius: 10px;
+    background: var(--green-color);
+    color: white;
+  }
+  .enidiv {
+    position: relative;
+    width: 100%;
+    height: 250px;
+  }
+  .bigdiv {
+    position: relative;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  .featuredtext {
+    font-weight: bold;
+  }
+
+  .featuredtext h1 {
+    font-size: 35px;
+    color: black;
+    font-weight: bolder;
+  }
+
+  @media (max-width: 768px) {
+    width: 95%;
+  margin: auto;
+  text-align: center;
+
+  .mainpopularsection {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
     align-items: center;
     margin-top: 40px;
   }
@@ -95,9 +166,7 @@ const Wrapper = styled.div`
     color: black;
     font-weight: bolder;
   }
-
-  @media (max-width: 768px) {
-    display: none;
+   
   }
 `;
 export default PopularCategories;
